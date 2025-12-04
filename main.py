37101a597380704516da9805936a928b788baca1
DSD-resilience <infantry97@gmail.com>
2025-12-04 10:53:21 -0500

    updated comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    # Use a breakpoint within the code line below to debug your script.
     print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
 
